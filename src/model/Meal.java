40b03deca435bc71b1b1fc07516ff4d664042e66
KillerNex <2115395065@qq.com>
2023-06-13 13:51:32 +0800

    增加：Meal

---
+++ src/model/Meal.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.math.BigDecimal;
+
+public class Meal {
+    int m_id;
+    String meal_name;
+    BigDecimal price;
+
+    public Meal(String meal_name, BigDecimal price) {
+        this.meal_name = meal_name;
+        this.price = price;
+    }
+
+    public int getM_id() {
+        return m_id;
+    }
+
+    public void setM_id(int m_id) {
+        this.m_id = m_id;
+    }
+
+    public String getMeal_name() {
+        return meal_name;
+    }
+
+    public void setMeal_name(String meal_name) {
+        this.meal_name = meal_name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
