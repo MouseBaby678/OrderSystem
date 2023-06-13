e8aca00685f65ffe58a5408d53b4a7b2d50a847e
KillerNex <2115395065@qq.com>
2023-06-13 19:26:47 +0800

    修改：DiningTable

--- src/model/DiningTable.java
+++ src/model/DiningTable.java
@@ -2,12 +2,10 @@ package model;
 
 public class DiningTable {
     int t_id;
-    int diners_num;
     int status;
 
-    public DiningTable(int t_id, int diners_num, int status) {
+    public DiningTable(int t_id, int status) {
         this.t_id = t_id;
-        this.diners_num = diners_num;
         this.status = status;
     }
 
@@ -17,14 +15,6 @@ public class DiningTable {
 
     public void setT_id(int t_id) {
         this.t_id = t_id;
-    }
-
-    public int getDiners_num() {
-        return diners_num;
-    }
-
-    public void setDiners_num(int diners_num) {
-        this.diners_num = diners_num;
     }
 
     public int getStatus() {
